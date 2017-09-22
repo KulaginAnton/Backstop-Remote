649170932cea2cbc38a645d31e8c8ae7e84f2a7c
Anton Kulahin <anku@sitecore.net>
2017-09-22 17:38:12 +0300

    backstop service

--- routes/service.js
+++ routes/service.js
@@ -6,12 +6,15 @@ var router = express.Router();
 /* GET service listing. */
 router.get('/', function(req, res, next) {
     let method = req.query.method || '',
+        filterVal = req.query.filter || "",
         backstopDef,
         answer = '';
+    debugger;
     console.log('---------> ' + method);
     if (method === 'reference') {
-        backstopDef = backstop('reference')
+        backstopDef = backstop('reference');
     } else if (method === 'approve') {
+        backstop('approve');
         backstopDef = Promise.resolve('Approve done');
     } else if (method === 'test') {
         backstopDef = backstop('test')
