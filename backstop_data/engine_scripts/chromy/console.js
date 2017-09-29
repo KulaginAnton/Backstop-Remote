05b68b88e7c0ada49ff54065a6d433b3960cefc5
Anton Kulahin <anku@sitecore.net>
2017-09-29 16:39:30 +0300

    added engine scripts and examples

---
+++ backstop_data/engine_scripts/chromy/console.js
@@ -0,0 +1,15 @@
+module.exports = function(chromy, scenario) {
+
+    console.log('12--------------')
+        // console.log(scenario.to)
+    console.log(scenario)
+    console.log('1--------------')
+    chromy
+        .console((text) => {
+            console.log('--------------')
+            console.log(text)
+            console.log('--------------')
+
+        })
+
+};
