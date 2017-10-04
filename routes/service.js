5f2aef2399e1d4c2c269b8e9fb14e255c46b9201
Anton Kulagin <titusja@yandex.ru>
2017-10-05 00:00:51 +0300

    updated report helper

--- routes/service.js
+++ routes/service.js
@@ -2,144 +2,8 @@ var express = require('express');
 var backstop = require('backstopjs');
 var router = express.Router();
 var reportHelper = require('./utils/report-helper.js');
-var configHelper = require('./utils/configuration-helper.js');
 
-var IS_DEBUG = false;
-var log = function(msg) {
-    IS_DEBUG && console.log(msg);
-}
-
-/* GET service listing. */
-
-
-// var getLatestTest = function() {
-//     fs.readdir('../backstop_data/var/', function(err, subfolder) {
-
-//     })
-// }
-
-// var getWholeResult = function() {
-//     return fs.readFileSync('../backstop_data/var/', function(err, subfolder) {
-
-//     })
-// }
-// var getTestScenarious = function() {
-//     return configHelper.getConfiguration('backstop_data/html_report/config.js', function(err, data) {
-//         if (err) throw res.send(err);
-//         return reportHelper.makeJSON(data).tests
-//     });
-// }
-// var isTestApplicable = function() {
-
-// }
-
-var RESULT_TEST_PATH = 'backstop_data/html_report/config.js';
-var TEST_RESULT_PATH = "backstop_data/html_report/config.js"
-
-
-var getAllScenarious = function(scenArray) {
-    return scenArray.map(function(test) {
-        return test.label
-    })
-}
-var getAllApplicableTest = function(rootTest) {
-    var configuration = JSON.parse(configHelper.getConfiguration());
-    var allScenarious = getAllScenarious(configuration.scenarios);
-
-    log('rootTest->' + rootTest);
-    log('--------------------------');
-    log('rootTest.test->' + rootTest.test);
-    return rootTest.tests.filter(function(value) {
-        log('value = ' + value.pair.label)
-        return allScenarious.indexOf(value.pair.label) > -1;
-    })
-}
-
-var getLatestTest = function() {
-    return reportHelper.makeJSON(reportHelper.getResultTest(RESULT_TEST_PATH));
-}
-var cusomTestReport = function() {
-    log('1customReport = ' + reportHelper.getResultTest());
-    log('2customReport = ' + reportHelper.makeJSON(
-        reportHelper.getResultTest()
-    ));
-    return reportHelper.makeJSON(reportHelper.getResultTest());
-}
-
-var filterTestPairs = function(testPairs, filter) {
-    return testPairs.filter(function(value) {
-        log('filter 1 ->' + value.pair.label);
-        log('filter 1 ->' + filter.label);
-        log('filter 2 ->' + value.pair.reference);
-        log('filter 2 ->' + filter.reference);
-        return value.pair.label == filter.label && value.pair.reference == filter.reference;
-    })
-}
-
-var updateCustomReport = function(tests, latestRes) {
-    tests.forEach(function(currentValue, index, array) {
-        let updatedTest = filterTestPairs(latestRes.tests, {
-            label: currentValue.pair.label,
-            reference: currentValue.pair.reference
-        })
-        log(updatedTest);
-        if (updatedTest.length) {
-            array[index] = updatedTest[0];
-        }
-    })
-    return tests;
-}
-
-
-var copyResult = function() {
-    var latestRes = reportHelper.makeJSON(
-        reportHelper.getResultTest(RESULT_TEST_PATH)
-    );
-    log('copy-test = ' + reportHelper.getResultTest(RESULT_TEST_PATH));
-    reportHelper.saveReport(reportHelper.getResultTest(RESULT_TEST_PATH));
-}
-var getTestResult = function(update) {
-    var update = update || false;
-    if (update) {
-        copyResult();
-    } else {
-        startUpdate();
-    }
-}
-var updateResult = function(filter) {
-    log('filterVal = ' + filter)
-    if (filter) {
-        startUpdate();
-    } else {
-        copyResult()
-    }
-}
-
-var startUpdate = function() {
-    var latestRes = getLatestTest();
-    if (reportHelper.isReportExist()) {
-        var customReport = cusomTestReport();
-        //filtered tests
-        log('customReport-->' + customReport);
-        var tests = getAllApplicableTest(customReport);
-        log('applicable=' + tests)
-        var updatedTestRes = updateCustomReport(tests, latestRes);
-        customReport.tests = updatedTestRes;
-        log('12----------|>' + tests);
-        log('21----------|>' + updatedTestRes);
-        reportHelper.saveReport(
-            reportHelper.makeJSONP(JSON.stringify(customReport, null, ' '))
-        );
-    } else {
-        reportHelper.saveReport(
-            reportHelper.getResultTest()
-        );
-    }
-}
-
-
-
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     let method = req.query.method || '',
         filterVal = req.query.filter || "",
         backstopDef,
@@ -157,15 +21,13 @@ router.get('/', function(req, res, next) {
         })
     }
     backstopDef
-        .then(function(val) {
-            // log('1---------->' + val);
-            updateResult(filterVal)
+        .then(function (val) {
+            reportHelper.updateResult(filterVal)
             answer = 'Done ok';
             res.json({ "answer": answer })
         })
-        .catch(function(reason) {
-            //  log('2---------->' + reason);
-            updateResult(filterVal);
+        .catch(function (reason) {
+            reportHelper.updateResult(filterVal);
             answer = 'Error ' + reason;
             res.json({ "answer": answer })
         })
--- routes/utils/report-helper.js
+++ routes/utils/report-helper.js
@@ -1,26 +1,111 @@
 var fs = require('fs');
-var Report = function() {};
+var configHelper = require('./configuration-helper.js');
+
+
+var IS_DEBUG = false,
+    REPORT_PATH = "test-result.js",
+    TEST_RESULT_PATH = "backstop_data/html_report/config.js";
+
+var Report = function () { };
+var log = function (msg) {
+    IS_DEBUG && console.log(msg);
+}
 //TODO add checking for type of str
-var REPORT_PATH = "test-result.js";
-Report.prototype.makeJSON = function(str) {
+
+Report.prototype.makeJSON = function (str) {
     var report = str.replace(/^report\(/gm, '').replace(/\);$/gm, '');
     return JSON.parse(report);
 }
-Report.prototype.makeJSONP = function(str) {
-        var JSONP = 'report(' + str + ');';
-        return JSONP;
-    }
-    //return latest test result in JSONP format */
-Report.prototype.getResultTest = function(path, callback) {
-    var path = path || REPORT_PATH;
-    return fs.readFileSync(path, 'utf8');
-    //return this.makeJSON(str).scenarios;
+//return str to JSONP
+Report.prototype.makeJSONP = function (str) {
+    var JSONP = 'report(' + str + ');';
+    return JSONP;
 }
-Report.prototype.isReportExist = function() {
+//return latest test result in JSON format */
+Report.prototype.getResultTest = function (path) {
+    var path = path || REPORT_PATH;
+    // return fs.readFileSync(path, 'utf8');
+    return this.makeJSON(fs.readFileSync(path, 'utf8'));
+}
+var isReportExist = function () {
     return fs.existsSync(REPORT_PATH);
 }
-Report.prototype.saveReport = function(json) {
-    return fs.writeFileSync(REPORT_PATH, json, 'utf8')
+// save JSON as JSONP to report file
+Report.prototype.saveReport = function (json) {
+    return fs.writeFileSync(REPORT_PATH, this.makeJSONP(JSON.stringify(json, null, ' ')), 'utf8')
+}
+
+Report.prototype.getAllScenarious = function (scenArray) {
+    return scenArray.map(function (test) {
+        return test.label
+    })
+}
+Report.prototype.getAllApplicableTest = function (rootTest) {
+    var configuration = JSON.parse(configHelper.getConfiguration());
+    var allScenarious = this.getAllScenarious(configuration.scenarios);
+    log('rootTest->' + rootTest);
+    log('rootTest.test->' + rootTest.test);
+    return rootTest.tests.filter(function (value) {
+        log('value = ' + value.pair.label)
+        return allScenarious.indexOf(value.pair.label) > -1;
+    })
+}
+
+Report.prototype.filterTestPairs = function (testPairs, filter) {
+    return testPairs.filter(function (value) {
+        log('filter 1 ->' + value.pair.label);
+        log('filter 1 ->' + filter.label);
+        log('filter 2 ->' + value.pair.reference);
+        log('filter 2 ->' + filter.reference);
+        return value.pair.label == filter.label && value.pair.reference == filter.reference;
+    })
+}
+Report.prototype.updateCustomReport = function (tests, latestRes) {
+    var _this = this;
+    tests.forEach(function (currentValue, index, array) {
+        let updatedTest = _this.filterTestPairs(latestRes.tests, {
+            label: currentValue.pair.label,
+            reference: currentValue.pair.reference
+        })
+        log(updatedTest);
+        if (updatedTest.length) {
+            array[index] = updatedTest[0];
+        }
+    })
+    return tests;
+}
+
+
+Report.prototype.copyResult = function () {
+    var latestRes = this.getResultTest(TEST_RESULT_PATH);
+    log('copy-test = ' + latestRes);
+    this.saveReport(this.getResultTest(TEST_RESULT_PATH));
+}
+
+Report.prototype.startUpdate = function () {
+    if (isReportExist()) {
+        var latestResult = this.getResultTest(TEST_RESULT_PATH),
+            customReport = this.getResultTest();
+        log('customReport-->' + customReport);
+        var testList = this.getAllApplicableTest(customReport);
+        log('applicable=' + testList)
+        var updatedTestRes = this.updateCustomReport(testList, latestResult);
+        customReport.tests = updatedTestRes;
+        log('21----------|>' + updatedTestRes);
+        this.saveReport(customReport);
+    } else {
+        this.saveReport(
+            getResultTest()
+        );
+    }
+}
+Report.prototype.updateResult = function (filter) {
+    log('filterVal = ' + filter)
+    if (filter) {
+        this.startUpdate();
+    } else {
+        this.copyResult()
+    }
 }
 
 module.exports = new Report();
