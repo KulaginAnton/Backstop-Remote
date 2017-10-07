0f4c051082a2504a3615aef77bd39d6590a5010b
Anton Kulagin <titusja@yandex.ru>
2017-10-07 23:50:58 +0300

    removed unused files

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var report = require('./routes/report');
 var service = require('./routes/service');
-var forTest = require('./routes/for-test');
 var cors = require('cors')
 var app = express();
 
 app.use(cors())
-    // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -28,19 +26,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'backstop_data')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/report', report);
 app.use('/service', service);
-app.use('/for-test', forTest);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ public/javascripts/main.js
@@ -0,0 +1,3 @@
+// window.onerror = function (error, url, line) {
+//     alert({ acc: 'error', data: 'ERR:' + error + ' URL:' + url + ' L:' + line });
+// };
