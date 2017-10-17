c80f8a0710c97f861f5822a8e4dfce7bab0c0ea6
Anton Kulahin <anku@sitecore.net>
2017-10-17 17:45:02 +0300

    configuration helper update

--- routes/utils/configuration-helper.js
+++ routes/utils/configuration-helper.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
-var Configuration = function () { };
+var Configuration = function() {};
 //TODO add checking for type of str
 var CONF_PATH = "backstop.json";
 
-var _typeof = function (elem) {
+var _typeof = function(elem) {
     return Object.prototype.toString.call(elem)
 }
 
-var makeSelectorsArray = function (selectors) {
+var makeSelectorsArray = function(selectors) {
     var res = selectors;
     console.log(_typeof(selectors))
     if (_typeof(selectors) !== "[object Array]") {
@@ -17,25 +17,25 @@ var makeSelectorsArray = function (selectors) {
     return res;
 }
 
-var updateScenariosType = function (scenarios) {
-    scenarios.forEach(function (element, index, array) {
-        array[index] = makeSelectorsArray(array[index].selectors)
+var updateScenariosType = function(scenarios) {
+    scenarios.forEach(function(element, index, array) {
+        array[index].selectors = makeSelectorsArray(array[index].selectors)
     }, this);
     return scenarios;
 }
 
-Configuration.prototype.getConfiguration = function (path) {
+Configuration.prototype.getConfiguration = function(path) {
     var path = path || CONF_PATH;
     if (!fs.existsSync(path)) {
         throw new Error(`Configuration file ${path} doesn\`t exist`);
     }
     return fs.readFileSync(path, 'utf8');
 }
-Configuration.prototype.saveConfiguration = function (configuration, PATH_TO_FILE) {
+Configuration.prototype.saveConfiguration = function(configuration, PATH_TO_FILE) {
     var REPORT_PATH = PATH_TO_FILE || CONF_PATH;
-    return fs.writeFileSync(REPORT_PATH, JSON.stringify(configuration, null, ' '))
+    return fs.writeFileSync(REPORT_PATH, JSON.stringify(configuration, null, 6))
 }
-Configuration.prototype.updateScenarios = function (scenarios) {
+Configuration.prototype.updateScenarios = function(scenarios) {
     var path = path || CONF_PATH;
     var newConfig = JSON.parse(this.getConfiguration());
 
