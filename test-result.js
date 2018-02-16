ac81fbc99355d0d9a268ae844e282fae89f160d0
anton-kulagin <titusja@yandex.ru>
2018-02-16 15:19:45 +0200

    1.0.3 (#7)
    * updated readme, license and added demo tests
    
    * Update README.MD
    
    * Update README.MD
    
    * Update README.MD
    
    * Update README.MD
    
    fix typos
    
    * Update README.MD
    
    * #1.0.3 updated ui for server
    
    * #1.0.3 updated readme

--- test-result.js
+++ test-result.js
@@ -1,89 +1,51 @@
 report({
- "testSuite": "BackstopJS",
- "tests": [
-  {
-   "pair": {
-    "reference": "..\\bitmaps_reference\\backstop_default_Homepage-h1_0_h1_0_phone.png",
-    "test": "..\\bitmaps_test\\20171004-175415\\backstop_default_Homepage-h1_0_h1_0_phone.png",
-    "selector": "h1",
-    "fileName": "backstop_default_Homepage-h1_0_h1_0_phone.png",
-    "label": "Homepage-h1",
-    "misMatchThreshold": 0.1,
-    "diff": {
-     "isSameDimensions": false,
-     "dimensionDifference": {
-      "width": 0,
-      "height": -408
-     },
-     "misMatchPercentage": "2.14",
-     "analysisTime": 8,
-     "getDiffImage": null
-    },
-    "diffImage": "..\\bitmaps_test\\20171004-175415\\failed_diff_backstop_default_Homepage-h1_0_h1_0_phone.png"
-   },
-   "status": "fail"
-  },
-  {
-   "pair": {
-    "reference": "..\\bitmaps_reference\\backstop_default_Homepage-h1_0_h1_1_tablet.png",
-    "test": "..\\bitmaps_test\\20171004-175415\\backstop_default_Homepage-h1_0_h1_1_tablet.png",
-    "selector": "h1",
-    "fileName": "backstop_default_Homepage-h1_0_h1_1_tablet.png",
-    "label": "Homepage-h1",
-    "misMatchThreshold": 0.1,
-    "diff": {
-     "isSameDimensions": false,
-     "dimensionDifference": {
-      "width": 0,
-      "height": -68
-     },
-     "misMatchPercentage": "8.62",
-     "analysisTime": 10,
-     "getDiffImage": null
-    },
-    "diffImage": "..\\bitmaps_test\\20171004-175415\\failed_diff_backstop_default_Homepage-h1_0_h1_1_tablet.png"
-   },
-   "status": "fail"
-  },
-  {
-   "pair": {
-    "reference": "..\\bitmaps_reference\\backstop_default_Homepage-p_0_p_0_phone.png",
-    "test": "..\\bitmaps_test\\20171004-175448\\backstop_default_Homepage-p_0_p_0_phone.png",
-    "selector": "p",
-    "fileName": "backstop_default_Homepage-p_0_p_0_phone.png",
-    "label": "Homepage-p",
-    "misMatchThreshold": 0.1,
-    "diff": {
-     "isSameDimensions": true,
-     "dimensionDifference": {
-      "width": 0,
-      "height": 0
-     },
-     "misMatchPercentage": "0.00",
-     "getDiffImage": null
-    }
-   },
-   "status": "pass"
-  },
-  {
-   "pair": {
-    "reference": "..\\bitmaps_reference\\backstop_default_Homepage-p_0_p_1_tablet.png",
-    "test": "..\\bitmaps_test\\20171004-175448\\backstop_default_Homepage-p_0_p_1_tablet.png",
-    "selector": "p",
-    "fileName": "backstop_default_Homepage-p_0_p_1_tablet.png",
-    "label": "Homepage-p",
-    "misMatchThreshold": 0.1,
-    "diff": {
-     "isSameDimensions": true,
-     "dimensionDifference": {
-      "width": 0,
-      "height": 0
-     },
-     "misMatchPercentage": "0.00",
-     "getDiffImage": null
-    }
-   },
-   "status": "pass"
-  }
- ]
+      "testSuite": "BackstopJS",
+      "tests": [
+            {
+                  "pair": {
+                        "reference": "..\\bitmaps_reference\\WikiTests_Wiki-Home_0_document_0_desktop.png",
+                        "test": "..\\bitmaps_test\\20180202\\WikiTests_Wiki-Home_0_document_0_desktop.png",
+                        "selector": "document",
+                        "fileName": "WikiTests_Wiki-Home_0_document_0_desktop.png",
+                        "label": "Wiki-Home",
+                        "requireSameDimensions": "",
+                        "misMatchThreshold": "0.1",
+                        "error": "Reference file not found C:\\work\\test-tool\\service-server\\backstop_data\\bitmaps_reference\\WikiTests_Wiki-Home_0_document_0_desktop.png"
+                  },
+                  "status": "fail"
+            },
+            {
+                  "pair": {
+                        "reference": "..\\bitmaps_reference\\WikiTests_Wiki-Home-Footer_0_footer_0_desktop.png",
+                        "test": "..\\bitmaps_test\\20180202\\WikiTests_Wiki-Home-Footer_0_footer_0_desktop.png",
+                        "selector": ".footer",
+                        "fileName": "WikiTests_Wiki-Home-Footer_0_footer_0_desktop.png",
+                        "label": "Wiki-Home-Footer",
+                        "requireSameDimensions": "",
+                        "misMatchThreshold": "0.1",
+                        "diff": {
+                              "isSameDimensions": true,
+                              "dimensionDifference": {
+                                    "width": 0,
+                                    "height": 0
+                              },
+                              "misMatchPercentage": "0.00"
+                        }
+                  },
+                  "status": "pass"
+            },
+            {
+                  "pair": {
+                        "reference": "..\\bitmaps_reference\\WikiTests_Wiki-Home-Input_0_document_0_desktop.png",
+                        "test": "..\\bitmaps_test\\20180202\\WikiTests_Wiki-Home-Input_0_document_0_desktop.png",
+                        "selector": "document",
+                        "fileName": "WikiTests_Wiki-Home-Input_0_document_0_desktop.png",
+                        "label": "Wiki-Home-Input",
+                        "requireSameDimensions": "",
+                        "misMatchThreshold": "0.1",
+                        "error": "Reference file not found C:\\work\\test-tool\\service-server\\backstop_data\\bitmaps_reference\\WikiTests_Wiki-Home-Input_0_document_0_desktop.png"
+                  },
+                  "status": "fail"
+            }
+      ]
 });
